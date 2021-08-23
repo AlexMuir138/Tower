713461df3bb4f59a55ee12e7e2a81bb147830a87
Alex <muirkat138@gmail.com>
2021-08-23 15:56:42 -0600

    mangey transients

--- Tower/server/db/DbContext.js
+++ Tower/server/db/DbContext.js
@@ -1,10 +1,14 @@
 import mongoose from 'mongoose'
 import { Value as ValueSchema } from '../models/Value'
 import { AccountSchema } from '../models/Account'
+import EventSchema from '../models/Event'
+import TicketSchema from '../models/Ticket'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+  Event = mongoose.model('Event', EventSchema);
+  Ticket = mongoose.model('Ticket', TicketSchema);
 }
 
 export const dbContext = new DbContext()
