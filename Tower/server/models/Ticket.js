74fe8c230a8918a4d5f37375d0b1cd5aff64b6ec
Alex <muirkat138@gmail.com>
2021-08-23 15:56:30 -0600

    ticket model change

--- Tower/server/models/Ticket.js
+++ Tower/server/models/Ticket.js
@@ -1,11 +1,14 @@
-export class Ticket {
-  constructor(data) {
-    this.body = data.body
-    this.eventId = data.eventId
-    this.id = data.id
-    this.picture = data.picture
-    this.email = data.email
-    this.creator = data.creator
-    this.creatorId = data.creatorId
-  }
-}
+import mongoose, { Schema } from 'mongoose'
+
+const TicketSchema = new mongoose.Schema({
+  body: { type: String, required: true },
+  event: { type: Schema.Types.ObjectId, ref: 'Event', required: true },
+  creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true }
+}, { timestamps: true, toJSON: { virtuals: true } })
+TicketSchema.virtual('creator', {
+  localField: 'creatorId',
+  ref: 'Account',
+  foreignField: '_id',
+  justOne: true
+})
+export default TicketSchema
