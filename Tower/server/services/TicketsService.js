a35c43b27ea8d1f8d736e4b3cf7cd6e1dfd06ff7
Alex <muirkat138@gmail.com>
2021-08-27 21:17:31 -0600

    ticket service

--- Tower/server/services/TicketsService.js
+++ Tower/server/services/TicketsService.js
@@ -2,12 +2,22 @@ import { dbContext } from '../db/DbContext'
 import { BadRequest } from '../utils/Errors'
 
 class TicketsService {
-  createTicket(ticketData) {
-    throw new Error('Method not implemented.')
+  async createTicket(ticketData) {
+    const newTicket = await dbContext.Ticket.create(ticketData)
+    newTicket.populate('creator', 'name email picture')
+    return newTicket
   }
 
-  deleteTicket(ticketId) {
-    throw new Error('Method not implemented.')
+  async deleteTicket(ticketId) {
+    return await dbContext.Ticket.findByIdAndDelete(ticketId)
+  }
+
+  async getTicketById(id) {
+    const ticket = await dbContext.Ticket.findOne({ _id: id }).populate('creator')
+    if (!ticket) {
+      throw new BadRequest('Could not find')
+    }
+    return ticket
   }
 }
 export const ticketsService = new TicketsService()
