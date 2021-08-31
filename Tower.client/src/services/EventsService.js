0c6377e828003ba21934e3cff124b9c47705783f
Alex <muirkat138@gmail.com>
2021-08-30 23:16:52 -0600

    events service

--- Tower.client/src/services/EventsService.js
+++ Tower.client/src/services/EventsService.js
@@ -8,6 +8,23 @@ class EventsService {
     AppState.events = res.data.map(e => new Event(e))
   }
 
+  async getEventById(id) {
+    const res = await api.get(`api/events/${id}`)
+    AppState.activeEvent = res.data
+    logger.log(res.data)
+  }
+
+  async createEvent(eventData) {
+    const res = await api.post('api/events', eventData)
+    AppState.events.push(new Event(res.data))
+    return res.data
+  }
+
+  async editEvent(edit) {
+    const res = await api.put(`api/events/${edit.bug}`, edit)
+    return res.data
+  }
+
   async cancelEvent(id) {
     const res = await api.delete(`api/events/${id}`)
     logger.log(res)
