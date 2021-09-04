0bcd1aa80578cce463a55b2ed09e57e4ec4fc8c1
Alex <muirkat138@gmail.com>
2021-09-03 19:29:55 -0600

    env change

--- Tower.client/src/pages/HomePage.vue
+++ Tower.client/src/pages/HomePage.vue
@@ -1,9 +1,6 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo">
-    <h1 class="my-5 bg-dark text-light p-3 rounded d-flex align-items-center">
-      This is where the party starts!
-    </h1>
+  <div class="home container-fluid">
+    Here Be The Events
   </div>
 </template>
 
